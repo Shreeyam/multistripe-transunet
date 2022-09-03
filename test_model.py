a10fdd620df3b41b960e9571b7933f3f34ff425b
shreeyam <shreeyam@mit.edu>
2022-09-03 13:02:45 -0700

    added some summary and ploy stuff

--- test_model.py
+++ test_model.py
@@ -1,8 +1,13 @@
 import torch
 from transunet import *
 from torchsummary import summary
+import matplotlib.pyplot as plt
 
 model = TransUNet()
 img = torch.randn((1, 1, 224, 224))
-print(model(img))
+out = (model(img))
+print(out)
 summary(model, (1, 224, 224))
+
+plt.imshow(out[0][0].detach().numpy())
+plt.show()
